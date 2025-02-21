e44a929f685de61060aab40c4a9ca5bccc643a43
Yaseen-A-Khan <121960551+Yaseen-A-Khan@users.noreply.github.com>
2025-02-21 18:37:30 +0530

    Update LandingPage.js

--- myresearchwebsite/src/pages/LandingPage.js
+++ myresearchwebsite/src/pages/LandingPage.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram } from "react-icons/fa"; // Import social media icons
 import HeroVideo from "../components/HeroVideo";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Cards from "../components/Cards";
 
 export default function LandingPage() {
   return (
@@ -10,9 +10,8 @@ export default function LandingPage() {
   <div>
     <Header />
       <main className="flex-1 relative top-10">   
-      <HeroVideo />
-        <div className=" min-h-72"></div>
-        <div className=" min-h-72"></div>
+        <HeroVideo />
+        <Cards />
       </main>
       
 
